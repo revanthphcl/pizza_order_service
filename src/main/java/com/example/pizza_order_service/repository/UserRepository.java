e870820c0b9e566a167c6ebd64340bd645964eae
johncaley <60158126+johncaley@users.noreply.github.com>
2021-08-05 14:16:50 -0400

    create user service

--- src/main/java/com/example/pizza_order_service/repository/UserRepository.java
+++ src/main/java/com/example/pizza_order_service/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.example.pizza_order_service.repository;
 
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.pizza_order_service.model.User;
 
 public interface UserRepository extends CrudRepository<User, Long>{
-
+	
+	public User findByFirstName(String firstName);
+	public User findByuID(Long integer);
+	
 }
