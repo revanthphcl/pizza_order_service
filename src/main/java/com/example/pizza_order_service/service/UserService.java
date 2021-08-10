36e900a99a1933015dec82df600c2e2783a255af
David Mauriello <dmauriello82@gmail.com>
2021-08-10 14:56:25 -0400

    Spring Security basics
    Minimal code to get spring security working, needs to merge with UserService for proper functionality.

---
+++ src/main/java/com/example/pizza_order_service/service/UserService.java
@@ -0,0 +1,14 @@
+package com.example.pizza_order_service.service;
+
+import org.springframework.stereotype.Service;
+
+import com.example.pizza_order_service.model.User;
+
+@Service
+public class UserService {
+
+	public User findUserByUserName(String username) {
+		return null;
+	}
+
+}
