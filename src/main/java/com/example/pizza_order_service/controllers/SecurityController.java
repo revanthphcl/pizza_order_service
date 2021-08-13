2a05414820ffcce3cc7a2ae2c4ecda1093e0d856
gvillar08 <78232517+gvillar08@users.noreply.github.com>
2021-08-13 15:00:30 -0700

    Controller now shows a page
    The page shown refuses to load images and stylesheets, so this is very much a WIP

--- src/main/java/com/example/pizza_order_service/controllers/SecurityController.java
+++ src/main/java/com/example/pizza_order_service/controllers/SecurityController.java
@@ -2,8 +2,10 @@ package com.example.pizza_order_service.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.pizza_order_service.model.User;
@@ -15,28 +17,33 @@ public class SecurityController {
 	@Autowired
 	UserService userService;
 	
-	@GetMapping("/login")
-	public ModelAndView getLoginPage() {
-		User user = new User();
-		return new ModelAndView("login", "fn5", user);       
+	@GetMapping({"/","/login"})
+	public String mainRedirect() {
+		return "login.html";
 	}
 	
-	@GetMapping("/registration")
-	public ModelAndView getCreateUser() {
-		User user = new User();
-		return new ModelAndView("createUser", "fn6", user);
-	}
-	
-	@PostMapping("/registration")
-	public ModelAndView postCreateUser(User user) {
-		boolean exists = userService.userExists(user);
-		if (exists) {
-			String message = "User name already exists!";
-			return new ModelAndView("errorPage", "fn7", message);
-		} else {
-			userService.save(user);
-			return getLoginPage();			
-		}
-
-	}
+//	@GetMapping("/login")
+//	public ModelAndView getLoginPage() {
+//		User user = new User();
+//		return new ModelAndView("login", "fn5", user);
+//	}
+//	
+//	@GetMapping("/registration")
+//	public ModelAndView getCreateUser() {
+//		User user = new User();
+//		return new ModelAndView("createUser", "fn6", user);
+//	}
+//	
+//	@PostMapping("/registration")
+//	public ModelAndView postCreateUser(User user) {
+//		boolean exists = userService.userExists(user);
+//		if (exists) {
+//			String message = "User name already exists!";
+//			return new ModelAndView("errorPage", "fn7", message);
+//		} else {
+//			userService.save(user);
+//			return getLoginPage();			
+//		}
+//
+//	}
 }
