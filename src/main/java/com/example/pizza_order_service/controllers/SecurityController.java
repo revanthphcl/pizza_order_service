1d2fa1dd5d1f4a54e3538e7f3cfed347dd6f0a50
gvillar08 <78232517+gvillar08@users.noreply.github.com>
2021-08-19 19:43:25 -0700

    Frontend login controllers are complete

--- src/main/java/com/example/pizza_order_service/controllers/SecurityController.java
+++ src/main/java/com/example/pizza_order_service/controllers/SecurityController.java
@@ -2,8 +2,6 @@ package com.example.pizza_order_service.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,67 +22,38 @@ public class SecurityController {
 	}
 	
 	@GetMapping("/login")
-	public String load(Model model) {
-		model.addAttribute("id", "nope");
-		return "login.html";
-	}
-	
-	@PostMapping("/login")
-	public String login(@RequestParam(required=false) String username, @RequestParam(required=false) String pass, Model model) {
-		model.addAttribute("id", "nopeiddynope");
-		if(username!=null && pass!=null) {									//JS should handle nulls and whitespace...
-			User temp = userService.findUserByUserName(username);
-			if(temp!=null && pass.equals(temp.getPassword())) {
-				System.out.println("logged in!");
-				return "landing.html";
-			}
-			else {
-				System.out.println("user not found/invalid pass");
-				return "login.html";
-			}
+	public ModelAndView login(@RequestParam(required=false) String username, @RequestParam(required=false) String pass) {
+		if(username!=null && pass!=null) {														//only run the following if all fields are filled. Note that HTML handles incomplete field errors
+			User temp = userService.findUserByUserName(username);								//search for entered username
+			
+			if(temp==null)																		//check for invalid username
+				return new ModelAndView("login.html", "error", "Username not found");
+			
+			if(!temp.getPassword().equals(pass))												//check for incorrect password
+				return new ModelAndView("login.html", "error", "Incorrect password");
+			
+			return new ModelAndView("landing.html", "message", "Successfully signed in!");		//redirect user to landing on success
 		}
-		return "login.html";
+		return new ModelAndView("login.html");
 	}
 	
 	@GetMapping("/createAccount")
-	public String create(@RequestParam(required=false) String username, @RequestParam(required=false) String pass1, @RequestParam(required=false) String pass2) {
-		if(pass1!=pass2) {
+	public ModelAndView create(@RequestParam(required=false) String username, @RequestParam(required=false) String pass1, @RequestParam(required=false) String pass2) {
+		if(username!=null && pass1!=null && pass2!=null) {										//only run the following if all fields are filled. Note that HTML handles incomplete field errors
+			if(!pass1.equals(pass2))															//check for password mismatch
+				return new ModelAndView("createAccount.html", "error", "Please confirm your passwords match");
 			
-		}
-		if(username!=null && pass1!=null && pass2!=null) {					//JS should handle nulls and whitespace...
-			User user = new User();
+			User duplicate = userService.findUserByUserName(username);							//check for duplicate username
+			if(duplicate!=null)
+				return new ModelAndView("createAccount.html", "error", "Please choose a unique username");
+			
+			User user = new User();																//this is only run once above conditions are met
 			user.setName(username);
 			user.setPassword(pass1);
 			userService.save(user);
-			System.out.println("acct created. Redirecting...");
-			return "login.html";
+			return new ModelAndView("login.html", "message", "Account successfully created!");	//redirect user to login
 		}
 		else
-			return "createAccount.html";		
+			return new ModelAndView("createAccount.html");		
 	}
-	
-//	@GetMapping("/login")
-//	public ModelAndView getLoginPage() {
-//		User user = new User();
-//		return new ModelAndView("login", "fn5", user);
-//	}
-//	
-//	@GetMapping("/registration")
-//	public ModelAndView getCreateUser() {
-//		User user = new User();
-//		return new ModelAndView("createUser", "fn6", user);
-//	}
-//	
-//	@PostMapping("/registration")
-//	public ModelAndView postCreateUser(User user) {
-//		boolean exists = userService.userExists(user);
-//		if (exists) {
-//			String message = "User name already exists!";
-//			return new ModelAndView("errorPage", "fn7", message);
-//		} else {
-//			userService.save(user);
-//			return getLoginPage();			
-//		}
-//
-//	}
 }
