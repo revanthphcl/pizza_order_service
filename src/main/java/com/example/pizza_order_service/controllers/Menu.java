6c7f96fac2c6a61b4deb53a5300d52df965b0356
Sea Hungerford <sean.chris@hcl.com>
2021-08-30 16:14:47 -0500

    Menu
    Menu changed

--- src/main/java/com/example/pizza_order_service/controllers/Menu.java
+++ src/main/java/com/example/pizza_order_service/controllers/Menu.java
@@ -3,18 +3,19 @@ package com.example.pizza_order_service.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.example.pizza_order_service.model.User;
-import com.example.pizza_order_service.service.UserService;
+import com.example.pizza_order_service.service.ProductService;
 
 @Controller
 public class Menu {
 	
+	@Autowired
+    private ProductService prodService;
+	
 	@GetMapping("/menu")
-	public String contactUsRedirect() {
-		return "menu.html";
+	public ModelAndView contactUsRedirect() {
+		//System.out.println(prodService.findAll());
+		return new ModelAndView("menu.html", "menu", prodService.findAll());
 	}
 }
