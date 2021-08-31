7a8e1d5c7f1979d48eb18437ace31d483c8aff92
gvillar08 <78232517+gvillar08@users.noreply.github.com>
2021-08-31 10:46:29 -0700

    Functioning Menu

--- src/main/java/com/example/pizza_order_service/controllers/Menu.java
+++ src/main/java/com/example/pizza_order_service/controllers/Menu.java
@@ -1,10 +1,13 @@
 package com.example.pizza_order_service.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.example.pizza_order_service.model.Product;
 import com.example.pizza_order_service.service.ProductService;
 
 @Controller
@@ -15,7 +18,13 @@ public class Menu {
 	
 	@GetMapping("/menu")
 	public ModelAndView contactUsRedirect() {
-		//System.out.println(prodService.findAll());
-		return new ModelAndView("menu.html", "menu", prodService.findAll());
+		List<Product> products = prodService.findAll();
+		String output = "";
+		
+		for(int counter=0; counter<products.size(); counter++) {
+			Product currentProd = products.get(counter);
+			output+="<tr><td>"+currentProd.getName()+"</td><td>"+currentProd.getDescription()+"</td><td>"+currentProd.getPrice()+"</td></tr>";
+		}
+		return new ModelAndView("menu.html", "menu", output);
 	}
 }
