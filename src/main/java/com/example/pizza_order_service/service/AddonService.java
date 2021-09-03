9425c8ac351d294d738059e77b4f1cf176f4075e
David Mauriello <dmauriello82@gmail.com>
2021-09-03 10:38:09 -0400

    Update AddonService.java

--- src/main/java/com/example/pizza_order_service/service/AddonService.java
+++ src/main/java/com/example/pizza_order_service/service/AddonService.java
@@ -5,10 +5,12 @@ import java.util.Objects;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.example.pizza_order_service.model.Addon;
 import com.example.pizza_order_service.repository.AddonRepository;
 
+@Service
 public class AddonService {
 	
 	@Autowired
